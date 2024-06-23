04adc5b48bbc3ad2ad2c6aa5f82d50aada11bfcd
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-06-23 16:54:26 -0400

    Add more payload logging

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ def create_replacement_dict():
     domain = f"https://{github_repo}.github.io/"
     replacements = []
     payload = environ.get('PAYLOAD')
-    print("type0:" + type(payload))
-    print("0: " + payload)
+    print("type0:" + str(type(payload)))
+    print("0: " + str(payload))
     custom_data = json.loads(payload)
-    print("type1:" + type(custom_data))
-    print("1:" + custom_data)
+    print("type1:" + str(type(custom_data)))
+    print("1:" + str(custom_data))
     if 'data' in custom_data:
         data_list = custom_data['data']
-        print("type2:" + type(data_list))
-        print("2:" + data_list)
+        print("type2:" + str(type(data_list)))
+        print("2:" + str(data_list))
         for data in data_list:
             to = domain + data['to']
             replacements[data['from']] = to
